3d98054d444dd329b6f84cb4df0fd81a7d0031f3
WRockefeller <milancbiju@gmail.com>
2024-07-14 18:11:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-document.getElementById('bookingForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById('bookingForm').addEventListener('submit', function(e) {
+    e.preventDefault();
 
-    const day = document.getElementById('day').value;
-    const email = document.getElementById('email').value;
+    var day = document.getElementById('day').value;
+    var email = document.getElementById('email').value;
 
-    fetch('https://script.google.com/macros/s/AKfycbzcMjbxhsYDgPFivHGl0H4xZID5IuzGyHjT9drE3vtbT-L9109hU_4QKIaKXdOCJfyWBQ/exec', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({ day, email, time })
-    })
-    .then(response => response.text())
-    .then(data => {
-        alert(data);
-    })
-    .catch(error => {
-        alert('Error: ' + error.message);
-    });
+    if (day && email) {
+        fetch('https://script.google.com/macros/s/AKfycbzms4XY47V0QYR0Lwu90tBfTQo6JcITLby9yP-l8JUB84bPkNPQ35I7jDQ9JcW2J07N9w/exec', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ day: day, email: email }),
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    } else {
+        alert('Please fill out all fields.');
+    }
 });
+
 
 
 
