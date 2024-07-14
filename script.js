54e9061fab21715eae8de76a672cac12b9b24d59
WRockefeller <milancbiju@gmail.com>
2024-07-14 15:30:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('bookingForm').addEventListener('submit', function(event
     const day = document.getElementById('day').value;
     const email = document.getElementById('email').value;
 
-    fetch('https://script.google.com/macros/s/AKfycbzBq5z4-evFwaB0bJj0l-W4z19R7CaMcileFTkUC0Swb7yVVudvAoG3szP0Nn2jsIHe8g/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzcMjbxhsYDgPFivHGl0H4xZID5IuzGyHjT9drE3vtbT-L9109hU_4QKIaKXdOCJfyWBQ/exec', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
